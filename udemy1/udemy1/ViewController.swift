04c606cdc861b28f3d9d660b6ce3b40e816efaf3
David Santiago Fdez-Bermejo Morales <david@davidsantiago.net>
2016-02-29 19:03:01 +0100

    Inicio

---
+++ udemy1/udemy1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  udemy1
+//
+//  Created by David Santiago Fdez-Bermejo Morales on 29/2/16.
+//  Copyright © 2016 David Santiago Fdez-Bermejo Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBOutlet weak var boton1: UIButton!
+
+
+}
+

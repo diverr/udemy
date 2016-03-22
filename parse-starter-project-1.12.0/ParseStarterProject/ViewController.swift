bad518ccad98f80de2e3edff923e7f18779b5d01
David Santiago <david@davidsantiago.net>
2016-03-22 23:59:17 +0100

    Avanzando con Parse

--- parse-starter-project-1.12.0/ParseStarterProject/ViewController.swift
+++ parse-starter-project-1.12.0/ParseStarterProject/ViewController.swift
@@ -15,6 +15,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Parse.setApplicationId("y3h39K2E7w3NjlIp4tHcBQslM64svj0Zr59V2jNG", clientKey: "0xQIJhRlnYwZrBBWSG0Vt63NS0gRa6tFAVSkCdZK")
+        
+//        let score = PFObject(className: "score")
+//        
+//        score.setObject("David", forKey: "name")
+//        score.setObject(95, forKey: "number")
+//        
+//        score.saveInBackgroundWithBlock { (success, error) -> Void in
+//            if success {
+//                print("El objeto score ha sido creado con ID \(score.objectId!)")
+//            }
+//            else {
+//                print(error)
+//            }
+//        }
+        
+        
+        let query = PFQuery(className: "score")
+        query.getObjectInBackgroundWithId("LA1qpYwkaw") { (score: PFObject?, error) -> Void in
+            if error == nil {
+                score!["name"] = "Hola amigo"
+                score!["number"] = 53
+                
+                score?.saveInBackgroundWithBlock({ (success, error) -> Void in
+                    if success {
+                        print(score)
+                    }
+                    else {
+                        print(error)
+                    }
+                })
+            }
+            else {
+                print(error)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {

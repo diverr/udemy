98ab6ff2760c88a4e6183f96f350fd70e86be35e
David Santiago <david@davidsantiago.net>
2016-04-01 09:45:09 +0200

    Subida de publicaciones ok

--- Instagram2/ParseStarterProject/PostViewController.swift
+++ Instagram2/ParseStarterProject/PostViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Parse
 
 class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var photoSelected: Bool = false
     
@@ -16,7 +19,59 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
     
     @IBOutlet weak var shareText: UITextField!
     
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: { (action) in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func postImage(sender: AnyObject) {
+        var error = ""
+        
+        if !self.photoSelected {
+            error = "Por favor elige una imagen"
+        }
+        else if shareText.text == "" {
+            error = "Por favor escribe un texto"
+        }
+        
+        if error != "" {
+            self.displayAlert("Error en los datos", message: error)
+        }
+        else {
+            let post = PFObject(className: "Post")
+            post["title"] = shareText.text
+            
+            // vamos con la imagen
+            let imageData = UIImagePNGRepresentation(self.imageToPost.image!)
+            let imageFile = PFFile(name: "image.png", data: imageData!)
+            post["imageFile"] = imageFile
+            post["username"] = PFUser.currentUser()?.username
+            
+            self.activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            post.saveInBackgroundWithBlock({ (success, error) in
+                if success {
+                    self.displayAlert("Publicación completada", message: "Tu foto ha sido completada con éxito")
+                }
+                else {
+                    self.displayAlert("No se pudo publicar", message: "No se pudo subir los datos")
+                }
+                
+                self.photoSelected = false
+                self.shareText.text = ""
+                self.imageToPost.image = UIImage(named: "Silueta-MAS.jpg")
+                
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+            })
+        }
         
     }
     
@@ -37,7 +92,8 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.activityIndicator.stopAnimating()
         // Do any additional setup after loading the view.
     }
 
